2b74d7f45a0cab1e0efe485f2861db909b53511a
Pierce Brooks <piercebrooks@rocketmail.com>
2023-01-16 13:08:41 -0500

    allow for automatic discovery of processor architecture in C# example build script

--- examples/csharp/build.sh
+++ examples/csharp/build.sh
@@ -3,10 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 rm -f csharp
 rm -f libcsharp*.so
-dub build --arch=x86 --force
-mv libcsharp.so libcsharp.x86.so
-dub build --arch=x86_64 --force > /dev/null 2>&1
-mv libcsharp.so libcsharp.x64.so
+dub build --arch=`uname -m` --force > /dev/null 2>&1
+mv libcsharp.so libcsharp.`uname -m`.so
 rm -f libcsharp.so
 dub run --config=emitCSharp
 LD_LIBRARY_PATH=$DIR dotnet run
